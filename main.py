87c4e2c5f1017926e924133142bfe7f91706b19a
SalimRK <rizksalimrizk@gmail.com>
2024-10-09 22:19:59 +0300

    buttomAppBarr

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import flet as ft
+
+
+def main(page: ft.Page):
+    page.title = "RhymPy"
+    page.horizontal_alignment = page.vertical_alignment = "center"
+
+    page.floating_action_button = ft.FloatingActionButton(icon=ft.icons.PLAY_ARROW)
+    page.floating_action_button_location = ft.FloatingActionButtonLocation.CENTER_DOCKED
+
+    page.bottom_appbar = ft.BottomAppBar(
+        bgcolor=ft.colors.BLUE,
+        shape=ft.NotchShape.CIRCULAR,
+        content=ft.Row(
+            controls=[
+                ft.IconButton(icon=ft.icons.MENU, icon_color=ft.colors.WHITE),
+                ft.Container(expand=True),
+                ft.IconButton(icon=ft.icons.SEARCH, icon_color=ft.colors.WHITE),
+                ft.IconButton(icon=ft.icons.FAVORITE, icon_color=ft.colors.WHITE),
+            ]
+        ),
+    )
+
+    page.add(ft.Text("Body!"))
+
+
 if __name__ == '__main__':
-    print("greetings")
-    print('Hello World')
+    ft.app(main)
